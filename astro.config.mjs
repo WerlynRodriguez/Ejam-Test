7f21cf88ed37b3c321024ea33cbf50f107c12f76
Werlyn <rdwerlynjose.16@gmail.com>
2023-09-21 20:48:31 -0600

    correct info for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: 'https://WerlynRodriguez.github.io/EjamTest/',
+    site: 'https://WerlynRodriguez.github.io',
+    base: '/EjamTest',
 });
